9c54c4d5bfae6de3ce7dbc62995181b0bcf1b8e7
dehsimoes <andre.simoes.44@gmail.com>
2023-05-28 03:41:30 -0300

    first commit

---
+++ theming/jstack-styled-theming-base/src/App.js
@@ -0,0 +1,65 @@
+import React, { useState, useMemo, useEffect } from 'react';
+import { ThemeProvider } from 'styled-components';
+
+import GlobalStyle from './styles/global';
+import Layout from './components/Layout';
+
+import themes from './styles/themes';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      theme: 'dark',
+    }
+  }
+
+  render() {
+    const { theme } = this.state;
+
+    return (
+      <ThemeProvider theme={themes[theme] || themes.dark}>
+        <GlobalStyle />
+        <Layout 
+          onToggleTheme={() => {
+            this.setState(prevState => ({ theme: prevState.theme === 'dark' ? 'light' : 'dark'}));
+          }} 
+          selectedTheme={theme}
+        />
+      </ThemeProvider>
+    );
+  }
+}
+
+/* function App() {
+  const [theme, setTheme] = useState(() => {
+    const themeStore = localStorage.getItem('theme');
+    return themeStore !== null ? localStorage.getItem('theme') : 'dark';
+  });
+
+  const currentTheme = useMemo(() => {
+    return themes[theme] || themes.dark;
+  }, [theme]);
+
+  function handleToggleTheme() {
+    setTheme(prevState => prevState === 'dark' ? 'light' : 'dark');
+  }
+
+  useEffect(() => {
+    localStorage.setItem('theme', theme)
+  }, [theme]);
+
+  return (
+    <ThemeProvider theme={currentTheme}>
+      <GlobalStyle />
+      <Layout 
+        onToggleTheme={handleToggleTheme} 
+        selectedTheme={theme}
+      />
+    </ThemeProvider>
+  );
+};
+*/
+
+export default App;

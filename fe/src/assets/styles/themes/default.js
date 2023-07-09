afcbcb8bba63dcfe5e10d039f63255972ea9f496
dehsimoes <andre.simoes.44@gmail.com>
2023-07-09 14:03:33 -0300

    Create Modal and prop Button danger

--- fe/src/assets/styles/themes/default.js
+++ fe/src/assets/styles/themes/default.js
@@ -8,8 +8,13 @@ export default {
       dark: '#3346F0',
     },
     gray: {
-      900: '#22222',
+      900: '#222222',
       200: '#BCBCBC',
+    },
+    danger: {
+      light: '#F97171',
+      main: '#FC5050',
+      dark: '#F63131',
     },
   },
 };

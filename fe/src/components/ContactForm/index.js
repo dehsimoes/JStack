54cdccc775ff3d8ebfb369fb09283badb0bb04d1
dehsimoes <andre.simoes.44@gmail.com>
2023-07-12 00:18:37 -0300

    Init validate Form

--- fe/src/components/ContactForm/index.js
+++ fe/src/components/ContactForm/index.js
@@ -11,11 +11,16 @@ export default function ContactForm({ buttonLabel }) {
   return (
     <Form>
       <FormGroup>
-        <Input placeholder="Nome" />
+        <Input
+          placeholder="Nome"
+          onChange={(event) => console.log(event.target.value)}
+        />
       </FormGroup>
 
-      <FormGroup>
-        <Input placeholder="E-mail" />
+      <FormGroup
+        error="O formato do e-mail é inválido."
+      >
+        <Input placeholder="E-mail" error />
       </FormGroup>
 
       <FormGroup>

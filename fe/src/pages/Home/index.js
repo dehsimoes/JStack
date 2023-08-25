54447ab5c20ddba9614591005c66caa088caa2db
dehsimoes <andre.simoes.44@gmail.com>
2023-08-25 01:41:22 -0300

    Cached preflight and Parse reponse body

--- fe/src/pages/Home/index.js
+++ fe/src/pages/Home/index.js
@@ -58,13 +58,17 @@ export default function Home() {
 }
 
 fetch('http://localhost:3001/contacts', {
-  method: 'DELETE',
+  method: 'GET',
   headers: new Headers({
     'X-App-ID': '123',
   }),
 })
-  .then((response) => {
+  .then(async (response) => {
+    const json = await response.json();
     console.log('response', response);
+    json.forEach((contact) => {
+      console.log(contact.name);
+    });
   })
   .catch((error) => {
     console.log('erro', error);

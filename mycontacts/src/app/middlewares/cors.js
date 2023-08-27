1626294153bfc4c3220505b673df9f9ab19cb69b
dehsimoes <andre.simoes.44@gmail.com>
2023-08-27 03:41:21 -0300

    Allow Cors

--- mycontacts/src/app/middlewares/cors.js
+++ mycontacts/src/app/middlewares/cors.js
@@ -2,5 +2,6 @@ module.exports = (req, resp, next) => {
   resp.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
   resp.setHeader('Access-Control-Allow-Methods', '*');
   resp.setHeader('Access-Control-Allow-Headers', '*');
+  resp.setHeader('Access-Control-Max-Age', '10');
   next();
 };
